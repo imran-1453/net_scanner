462e135ebde0c22bae73f432cd2b685c6a9e4431
İmran İsa Gültekin <79415238+imran-1453@users.noreply.github.com>
2021-07-20 16:53:18 +0300

    Add files via upload

---
+++ my_net_scanner.py
@@ -0,0 +1,27 @@
+import scapy.all as scapy
+import optparse
+
+def input_object():
+
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i","--ip",dest="ip_adress",help="enter ip adress")
+    (user_input,arguments) = parse_object.parse_args()
+    if not user_input.ip_adress:
+        print("Enter ip adress")
+    return user_input.ip_adress
+
+def scan_my_network(ip_adress):
+
+    arp_requests_packet = scapy.ARP(pdst=ip_adress)
+
+    brodcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    combined_packet = brodcast_packet/arp_requests_packet
+
+    (answered_list,unanswered_list) = scapy.srp(combined_packet,timeout=1)
+
+    answered_list.summary()
+
+user_input_ip = input_object()
+
+scan_my_network(ip_adress=user_input_ip)
